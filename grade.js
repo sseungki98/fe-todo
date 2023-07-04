32d3a41f5571ef9efc41326eee3faa240711940b
seungki <sysh9498@gmail.com>
2023-07-04 17:23:35 +0900

    refactor: 함수명/변수명 리팩토링

--- grade.js
+++ grade.js
@@ -1,5 +1,6 @@
 const { todos } = require("./todos");
 const Validation = require("./validation");
+const { NOT_IN_STATUS_ERROR } = require("./errors");
 
 const readline = require("readline");
 const rl = readline.createInterface({
@@ -7,7 +8,50 @@ const rl = readline.createInterface({
   output: process.stdout,
 });
 
-function count_status() {
+var recursiveAsyncReadLine = function () {
+  rl.question(": ", function (answer) {
+    input = answer.split("$");
+    if (input[0] == "exit") {
+      return rl.close();
+    } else {
+      switch (input[0]) {
+        case "show":
+          if (input[1] == "all") {
+            showAll();
+          } else {
+            showStatus(input[1]);
+          }
+          break;
+        case "add":
+          tags = JSON.parse(input[2]);
+          addTodo(input[1], tags);
+          break;
+        case "delete":
+          deleteTodo(parseInt(input[1]));
+          break;
+        case "update":
+          updateTodo(parseInt(input[1]), input[2]);
+          break;
+        default:
+          console.log("올바른 명령어를 입력해주세요.");
+          console.log("-------------------------------------------------");
+          console.log("show$all => 모든 상태별 개수 확인");
+          console.log("show$[todo or doing or done] => 상태별 목록 확인");
+          console.log("add$[name]$[[tags]] => todo 리스트에 추가");
+          console.log("delete$[id] => todo에 존재하는 id 삭제");
+          console.log("update$[id]$[status] => id에 해당하는 일의 상태 업데이트");
+          console.log("exit => 끝내기");
+          console.log("-------------------------------------------------");
+          break;
+      }
+      recursiveAsyncReadLine();
+    }
+  });
+};
+
+recursiveAsyncReadLine();
+
+function countStatus() {
   // 개수 카운트 함수
   count = { todo: 0, doing: 0, done: 0 };
   todos.map(item => {
@@ -22,111 +66,80 @@ function count_status() {
         count["done"]++;
         break;
       default:
-        console.log("wrong input");
+        console.log(NOT_IN_STATUS_ERROR);
     }
   });
 
   return count;
 }
 
-function show_all() {
+function showAll() {
   // show$all
-  count = count_status();
+  count = countStatus();
   console.log(`현재상태 : todo: ${count["todo"]}개, doing: ${count["doing"]}개, done: ${count["done"]}개`);
 }
 
-function show_status(status) {
+function showStatus(status) {
   //show$['status']
-  const count = count_status();
-  const list = todos.filter(value => value.status == status);
+  const count = countStatus();
+  const filtered_todos_array = todos.filter(value => value.status == status);
   let name_id_list = "";
-  list.map(value => {
+  filtered_todos_array.map(value => {
     name_id_list += `'${value.name}, ${value.id.toString()}번'`;
   });
 
   console.log(`${status} 리스트 :  총${count[status]}건 : ` + `${name_id_list}`);
 }
 
-function add_todo(name, tags) {
+function getRandomID() {
+  return Math.floor(Math.random() * 10000);
+}
+
+function addTodo(name, tags) {
   //add$['name']$[['tags']]
   if (!Validation.checkNameExist(name)) {
     return;
   }
-  const id = Math.floor(Math.random() * 10000);
+  const id = getRandomID();
   while (!Validation.checkIdExist(id)) {
-    id = Math.floor(Math.random() * 10000);
+    id = getRandomID();
   }
-  const new_todo = {
+  todos.push({
     name: name,
     tags: tags,
     status: "todo",
     id: id,
-  };
-  todos.push(new_todo);
+  });
   console.log(`${name} 1개가 추가됐습니다. (id: ${id})`);
-  show_all();
+  showAll();
 }
 
-function delete_todo(id) {
+function deleteItem(value, index) {
+  console.log(`${value.name}가 목록에서 삭제되었습니다.`);
+  todos.splice(index, index + 1);
+  showAll();
+}
+
+function deleteTodo(id) {
   //delete$['id']
   todos.forEach((value, index) => {
-    if (value["id"] == id) {
-      if (Validation.checkStatus(value.status)) {
-        console.log(`${value.name}가 목록에서 삭제되었습니다.`);
-        todos.splice(index, index + 1);
-        show_all();
-        return false;
-      }
+    if (value["id"] === id && Validation.checkTodoStatus(value.status)) {
+      deleteItem(value, index);
     }
   });
 }
 
-function update_todo(id, status) {
+function updateItem(value, status) {
+  console.log(`${value.name}가 ${status}로 변경됐습니다.`);
+  value.status = status;
+  showAll();
+}
+
+function updateTodo(id, status) {
   //update$['id']$['status']
-  todos.forEach((value, index) => {
+  todos.forEach(value => {
     if (value["id"] == id && value["status"] != status) {
-      console.log(`${value.name}가 ${status}로 변경됐습니다.`);
-      value.status = status;
-      show_all();
-
-      return false;
+      updateItem(value, status);
     }
   });
 }
-
-rl.question("명령어를 입력하세요 > ", line => {
-  input = line.split("$");
-  if (input[0] === "exit") {
-    rl.close();
-  } else {
-    switch (input[0]) {
-      case "show":
-        if (input[1] == "all") {
-          show_all();
-        } else {
-          show_status(input[1]);
-        }
-        break;
-      case "add":
-        tags = JSON.parse(input[2]);
-        add_todo(input[1], tags);
-        break;
-      case "delete":
-        delete_todo(parseInt(input[1]));
-        break;
-      case "update":
-        update_todo(parseInt(input[1]), input[2]);
-        break;
-      default:
-        console.log("올바른 명령어를 입력해주세요.");
-        console.log("-------------------------------------------------");
-        console.log("show$all => 모든 상태별 개수 확인");
-        console.log("show$[todo or doing or done] => 상태별 목록 확인");
-        console.log("add$[name]$[[tags]] => todo 리스트에 추가");
-        console.log("delete$[id] => todo에 존재하는 id 삭제");
-        console.log("update$[id]$[status] => id에 해당하는 일의 상태 업데이트");
-        console.log("exit => 끝내기");
-        console.log("-------------------------------------------------");
-    }
-  }
-});
