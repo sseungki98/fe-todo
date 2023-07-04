828e8fb0e7277b53bff6a2b526f0a552b4b48e17
seungki <sysh9498@gmail.com>
2023-07-04 14:35:14 +0900

    feat: Validation_checkNameExist 추가

--- Validation.js
+++ Validation.js
@@ -1,21 +1,39 @@
 const { todos } = require("./todos");
+const { NO_ID_EXIST_ERROR, NOT_TODO_STATUS_ERROR, EXIST_NAME_ERROR } = require("./errors");
 
 class Validation {
   static checkIdExist(id) {
-    const id_list = todos.filter(value => value.id == id);
-    if (id_list.length == 0) {
+    const id_list = todos.filter(value => value.id === id);
+    if (id_list.length === 0) {
       return true;
-    } else {
-      return false;
     }
+
+    console.log(NO_ID_EXIST_ERROR);
+    return false;
   }
 
   static checkStatus(status) {
-    if (status == "todo") {
+    if (status === "todo") {
       return true;
-    } else {
-      return false;
     }
+    console.log(NOT_TODO_STATUS_ERROR);
+    return false;
+  }
+
+  static checkNameExist(name) {
+    let flag = true;
+    todos.some(value => {
+      if (value.name === name) {
+        console.log(EXIST_NAME_ERROR);
+        flag = false;
+        return true;
+      }
+    });
+    if (flag) {
+      return true;
+    }
+
+    return false;
   }
 }
 
