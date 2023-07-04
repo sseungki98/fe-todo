ccffaf893b628013d181eed93a2ab660168e542c
seungki <sysh9498@gmail.com>
2023-07-04 14:35:34 +0900

    feat: error 메세지 추가

---
+++ errors.js
@@ -0,0 +1,9 @@
+const NO_ID_EXIST_ERROR = "해당 ID가 존재하지 않습니다.";
+const NOT_TODO_STATUS_ERROR = "해당 ID에 해당하는 Status가 'todo'가 아닙니다.";
+const EXIST_NAME_ERROR = "TODO에 이미 존재하는 이름입니다.";
+
+module.exports = {
+  NO_ID_EXIST_ERROR,
+  NOT_TODO_STATUS_ERROR,
+  EXIST_NAME_ERROR,
+};
