546e320d236fe04c2e07691126045f70ee617c5a
seungki <sysh9498@gmail.com>
2023-07-04 11:17:07 +0900

    feat: 예외 제외한 add, delete, update 함수 추가

--- todos.js
+++ todos.js
@@ -21,10 +21,13 @@ const todos = [
 ];
 
 function show_all() {
-  console.log(`현재상태 : todo: ${count[0]}개, doing: ${count[1]}개, done: ${count[2]}개`);
+  // show$all
+  count = count_status();
+  console.log(`현재상태 : todo: ${count["todo"]}개, doing: ${count["doing"]}개, done: ${count["done"]}개`);
 }
 
 function count_status() {
+  // 개수 카운트 함수
   count = { todo: 0, doing: 0, done: 0 };
   todos.map(item => {
     switch (item.status) {
@@ -46,6 +49,7 @@ function count_status() {
 }
 
 function show_status(status) {
+  //show$['status']
   const count = count_status();
   const list = todos.filter(value => value.status == status);
   let name_id_list = "";
@@ -53,14 +57,69 @@ function show_status(status) {
     name_id_list += `'${value.name}, ${value.id.toString()}번'`;
   });
 
-  console.log(`${status}리스트 :  총${count[status]}건 : ` + `${name_id_list}`);
+  console.log(`${status} 리스트 :  총${count[status]}건 : ` + `${name_id_list}`);
+}
+
+function add(name, tags) {
+  const id = Math.floor(Math.random() * 10000);
+  const new_todo = {
+    name: name,
+    tags: tags,
+    status: "todo",
+    id: id,
+  };
+  todos.push(new_todo);
+  console.log(`${name} 1개가 추가됐습니다. (id: ${id})`);
+  show_all();
+}
+
+function delete_todo(id) {
+  todos.forEach((value, index) => {
+    if (value["id"] == id) {
+      console.log(`${value.name}가 목록에서 삭제되었습니다.`);
+      todos.splice(index, index + 1);
+      show_all();
+
+      return false;
+    }
+  });
+}
+
+function update(id, status) {
+  todos.forEach((value, index) => {
+    if (value["id"] == id && value["status"] != status) {
+      console.log(`${value.name}가 ${status}로 변경됐습니다.`);
+      value.status = status;
+      show_all();
+
+      return false;
+    }
+  });
 }
 
 let input = [];
 
 rl.on("line", line => {
   input = line.split("$");
-  //   console.log(count_status());
-  show_status("todo");
+  switch (input[0]) {
+    case "show":
+      if (input[1] == "all") {
+        show_all();
+      } else {
+        show_status(input[1]);
+      }
+      break;
+    case "add":
+      let tags = input[2]
+        .substring(1, input[2].length - 1)
+        .replace(/"/g, "")
+        .split(",");
+      add(input[1], tags);
+    case "delete":
+      delete_todo(parseInt(input[1]));
+    case "update":
+      update(parseInt(input[1]), input[2]);
+  }
+
   rl.close();
 });
