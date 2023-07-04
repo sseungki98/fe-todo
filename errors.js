2ccbf77a93ab5829b8a3dc3747603afddc7cabe2
seungki <sysh9498@gmail.com>
2023-07-04 17:24:34 +0900

    feat: 에러 메세지 추가

--- errors.js
+++ errors.js
@@ -1,9 +1,11 @@
 const NO_ID_EXIST_ERROR = "해당 ID가 존재하지 않습니다.";
 const NOT_TODO_STATUS_ERROR = "해당 ID에 해당하는 Status가 'todo'가 아닙니다.";
 const EXIST_NAME_ERROR = "TODO에 이미 존재하는 이름입니다.";
+const NOT_IN_STATUS_ERROR = "todo, doing, done 중 하나를 입력해주세요.";
 
 module.exports = {
   NO_ID_EXIST_ERROR,
   NOT_TODO_STATUS_ERROR,
   EXIST_NAME_ERROR,
+  NOT_IN_STATUS_ERROR,
 };
