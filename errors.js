15016e1699e4e18c133fa1faf27cf1978c93c84c
seungki <sysh9498@gmail.com>
2023-07-05 10:33:15 +0900

    feat: error 메세지 추가(NOT_JSON_ERROR)

--- errors.js
+++ errors.js
@@ -2,10 +2,12 @@ const NO_ID_EXIST_ERROR = "해당 ID가 존재하지 않습니다.";
 const NOT_TODO_STATUS_ERROR = "해당 ID에 해당하는 Status가 'todo'가 아닙니다.";
 const EXIST_NAME_ERROR = "TODO에 이미 존재하는 이름입니다.";
 const NOT_IN_STATUS_ERROR = "todo, doing, done 중 하나를 입력해주세요.";
+const NOT_JSON_ERROR = '입력된 Tags가 JSON 형식이 아닙니다. ["favorite","hobby"] 형식으로 입력해주세요.';
 
 module.exports = {
   NO_ID_EXIST_ERROR,
   NOT_TODO_STATUS_ERROR,
   EXIST_NAME_ERROR,
   NOT_IN_STATUS_ERROR,
+  NOT_JSON_ERROR,
 };
